71187c72ffaf6b393aaf27e1135c2bfaed807256
Gokul Kumar <gokulkumar0307@gmail.com>
2026-05-09 17:53:41 +0530

    terraform-sg

--- 10.sg/main.tf
+++ 10.sg/main.tf
@@ -1,10 +1,19 @@
-# Using Open source module
- module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
+# # Using Open source module
+#  module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "${local.common_name_suffix}-catalogue"
-  use_name_prefix = false
-  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+#   name        = "${local.common_name_suffix}-catalogue"
+#   use_name_prefix = false
+#   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
+# }
+
+module "sg" {
+    source = "git::https://github.com/My-Roboshop/terraform-aws-sg.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mongodb"
+    sg_description = "Created for mongodb"
+    vpc_id = local.vpc_id
 }
